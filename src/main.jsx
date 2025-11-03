b687f196131c0382ea22e7ee6dd4ec32faad1026
GameDevMelll <shi8344a@gmail.com>
2025-11-03 03:41:27 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // если у тебя есть этот файл; если нет — можешь удалить эту строку
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
