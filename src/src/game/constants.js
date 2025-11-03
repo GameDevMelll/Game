584736650af1aa09ecc775620fddcb9522fbcf74
GameDevMelll <shi8344a@gmail.com>
2025-11-03 03:54:24 +0000

    Update constants.js

--- src/src/game/constants.js
+++ src/src/game/constants.js
@@ -1,4 +1,3 @@
-// src/game/constants.js
 export const WORLD = { w: 2400, h: 1800 };
 
 export const PLAYER_SPEED = 230;
@@ -18,7 +17,6 @@ export const ZOMBIE_HARD_CAP = 50;
 export const MAZE_PASSAGE = PLAYER_DIAM * 2;
 export const WALL_THICKNESS = 42;
 
-// shooters
 export const WHITE_START_AT = 30;
 export const WHITE_SPAWN_EVERY = 4.0;
 export const WHITE_SPAWN_MIN = 1.0;
@@ -27,10 +25,8 @@ export const WHITE_MAX_AGE = 20;
 export const WHITE_HARD_CAP = 20;
 export const ENEMY_BULLET_SPEED = 380;
 
-// mines
 export const MINE_EXPLOSION_RADIUS = 128;
 
-// day/night
 export const DAY_DURATION = 60;
 export const NIGHT_DURATION = 60;
 export const DAY_NIGHT_CYCLE = DAY_DURATION + NIGHT_DURATION;
