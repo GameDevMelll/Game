d33fdb4e51c54b1d466c95bf494471a5a4926f34
GameDevMelll <shi8344a@gmail.com>
2025-11-03 03:44:44 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import Game from "./App.jsx"; // у тебя в канвасе export default function Game() { ... }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>
 );
