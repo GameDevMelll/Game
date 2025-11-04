9983cddc9294f08dae4302f9dbceb2216904e359
GameDevMelll <shi8344a@gmail.com>
2025-11-04 08:15:50 +0000

    Update constants.js

--- src/game/constants.js
+++ src/game/constants.js
@@ -2,9 +2,9 @@
 export const WORLD = { w: 2400, h: 1800 };
 
 // игрок
-export const PLAYER_SPEED = 230;
+export const PLAYER_SPEED = 530;
 export const PLAYER_DIAM = 34;
-export const PLAYER_MAX_HP = 100;
+export const PLAYER_MAX_HP = 1000000;
 export const PLAYER_LEVEL_HP_BONUS = 120;
 export const PLAYER_MEDKIT_HEAL = 35;
 export const MELEE_RANGE = 55;
