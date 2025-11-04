53b296128a2aea21aed91ec75598acbbed22d32e
GameDevMelll <shi8344a@gmail.com>
2025-11-04 11:21:25 +0000

    Update assetManifest.js

--- src/game/assetManifest.js
+++ src/game/assetManifest.js
@@ -4,9 +4,10 @@ import zombiePng from "../assets/textures/zom-removebg-preview.png"; // твой
 import GhostPng from "../assets/textures/Ghost-removebg-preview.png";
 import RangedPng from "../assets/textures/Ranged-removebg-preview.png";
 import axePng from "../assets/textures/axe.png";
-import bombPng from "../assets/textures/bomb.png";
+import bombPng from "../assets/textures/boom.png";
 import catPng from "../assets/textures/cat.png";
 import grenadePng from "../assets/textures/grenade.png";
+import gutlingPng from "../assets/textures/gutling.png";
 import machinegunPng from "../assets/textures/machinegun.png";
 import mineIdlePng from "../assets/textures/mine_idle.png";
 import mineArmedPng from "../assets/textures/mine_armed.png";
@@ -14,22 +15,30 @@ import pitchforkPng from "../assets/textures/pitchfork.png";
 import shieldPng from "../assets/textures/shield.png";
 import medkitIconPng from "../assets/textures/medkit_icon.png";
 import backPng from "../assets/textures/back2.png";
+import skelPng from "../assets/textures/skel.png";
+import rabitPng from "../assets/textures/rabit.png";
+import orcPng from "../assets/textures/orc.png";
+import mellPng from "../assets/textures/mell.png";
+import lomPng from "../assets/textures/lom.png";
+import gunPng from "../assets/textures/gun.png";
+import fogPng from "../assets/textures/fog.png";
+import bomPng from "../assets/textures/bom.png";
 import bembMp3 from "../assets/music/bemb.mp3"; //
 
 export const TEXTURE_MANIFEST = {
   [TEXTURE_KEYS.BACKGROUND]: backPng,
-  [TEXTURE_KEYS.WALL]: null,
+  [TEXTURE_KEYS.WALL]: wallPng,
   [TEXTURE_KEYS.PLAYER]: null,
-  [TEXTURE_KEYS.VILLAGER]: null,
-  [TEXTURE_KEYS.ZOMBIE]: zombiePng,        // ← ЗДЕСЬ!
+  [TEXTURE_KEYS.VILLAGER]: mellPng,
+  [TEXTURE_KEYS.ZOMBIE]: zombiePng,      
   [TEXTURE_KEYS.RANGED]: RangedPng,
   [TEXTURE_KEYS.GHOST]: GhostPng,
-  [TEXTURE_KEYS.SKELETON]: null,
+  [TEXTURE_KEYS.SKELETON]: skelPng,
   [TEXTURE_KEYS.WITCH]: RangedPng,
-  [TEXTURE_KEYS.BOMBER]: null,
-  [TEXTURE_KEYS.BOSS]: null,
-  [TEXTURE_KEYS.BOSS2]: null,
-  [TEXTURE_KEYS.BOSS3]: null,
+  [TEXTURE_KEYS.BOMBER]: bomPng,
+  [TEXTURE_KEYS.BOSS]: orcPng,
+  [TEXTURE_KEYS.BOSS2]: fogPng,
+  [TEXTURE_KEYS.BOSS3]: rabitPng,
   [TEXTURE_KEYS.PROJECTILE_PLAYER]: null,
   [TEXTURE_KEYS.PROJECTILE_ENEMY]: null,
   [TEXTURE_KEYS.SLASH]: null,
@@ -39,11 +48,11 @@ export const TEXTURE_MANIFEST = {
   [TEXTURE_KEYS.MINE_IDLE]: mineIdlePng,
   [TEXTURE_KEYS.MINE_ARMED]: mineArmedPng,
   [TEXTURE_KEYS.BOMB]: bombPng,
-  [TEXTURE_KEYS.SHOTGUN]: null,
+  [TEXTURE_KEYS.SHOTGUN]: machinegunPng,
   [TEXTURE_KEYS.GLAIVE]: pitchforkPng,
-  [TEXTURE_KEYS.PISTOL]: null,
-  [TEXTURE_KEYS.BAT]: null,
-  [TEXTURE_KEYS.MACHINEGUN]: machinegunPng,
+  [TEXTURE_KEYS.PISTOL]: gunPng,
+  [TEXTURE_KEYS.BAT]: lomPng,
+  [TEXTURE_KEYS.MACHINEGUN]: gutlingPng,
   [TEXTURE_KEYS.PITCHFORK]: pitchforkPng,
   [TEXTURE_KEYS.GRENADE]: grenadePng,
   [TEXTURE_KEYS.SHIELD]: shieldPng,
